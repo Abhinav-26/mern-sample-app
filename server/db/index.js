5e93cd9c99649a1865f488faafcff3abbbafd047
gunish-dt <gunish@devtron.ai>
2022-02-17 11:22:22 +0530

    code changes to include mongo

--- server/db/index.js
+++ server/db/index.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const connectionString = 'mongodb://mongo:27017/cinema';
+const connectionString = "mongodb://mongo.gunish:27017/cinema";
 
 mongoose.connect(connectionString, { useNewUrlParser: true }).catch((e) => {
-  console.error('Connection error', e.message);
+  console.error("Connection error", e.message);
 });
 
 const db = mongoose.connection;
